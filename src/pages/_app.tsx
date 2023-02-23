fe79aa56c29a2d69d9012f2b6a1eb3fb18373328
jkrumm <jkrumm@pm.me>
2023-02-23 23:31:41 +0100

    mantine ui basic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,27 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+      }}
+    >
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </MantineProvider>
   );
 };
 
