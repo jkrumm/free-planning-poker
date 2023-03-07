f82b4b12931694d4a5be8312c28868b61cc5fdb5
jkrumm <jkrumm@pm.me>
2023-03-07 09:25:42 +0100

    fix uuid package bug

--- src/store/local-storage.ts
+++ src/store/local-storage.ts
@@ -1,18 +1,3 @@
-import { v4 } from "uuid";
-
-export function getClientId(): string {
-  if (typeof window == "undefined") {
-    throw new Error("not on local");
-  } else {
-    let clientId = localStorage.getItem("userId");
-    if (!clientId) {
-      clientId = v4();
-      localStorage.setItem("userId", clientId);
-    }
-    return clientId;
-  }
-}
-
 export function getUsername(): string | null {
   if (typeof window == "undefined") {
     return null;
