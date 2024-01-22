0fcbe3cf8acbbd81e1a5c6726c46cabbd9df398b
jkrumm <jkrumm@pm.me>
2024-01-22 19:48:58 +0100

    feat(analytics): seperated user read model sync

--- analytics/scripts/update_read_model.py
+++ analytics/scripts/update_read_model.py
@@ -5,19 +5,23 @@ import pandas as pd
 from util.log_util import logger
 
 connection = MySQLdb.connect(
-    host=os.getenv("DB_HOST"),
+    host="aws.connect.psdb.cloud",
     user=os.getenv("DB_USERNAME"),
     passwd=os.getenv("DB_PASSWORD"),
-    db=os.getenv("DB_NAME"),
+    db="database",
     autocommit=True,
     ssl_mode="VERIFY_IDENTITY",
     ssl={
-        "ca": "/etc/pki/tls/certs/ca-bundle.crt"
+        "ca": os.getenv("DB_SSL")
     }
 )
 
 
 def upsert_table(cursor, table_name, dtypes_def):
+    if table_name == "fpp_users":
+        upsert_users(cursor)
+        return
+
     parquet_file = f"./data/{table_name}.parquet"
 
     # Load existing Parquet file (if exists)
@@ -31,12 +35,7 @@ def upsert_table(cursor, table_name, dtypes_def):
         df_parquet = pd.DataFrame()
         last_id = 0
 
-    # TODO: improve user update logic
-    # Read new data from MySQL
-    if table_name != "fpp_users":
-        cursor.execute(f"SELECT * FROM {table_name} WHERE id > {last_id}")
-    else:
-        cursor.execute("SELECT * FROM fpp_users ORDER BY created_at DESC")
+    cursor.execute(f"SELECT * FROM {table_name} WHERE id > {last_id}")
 
     missing_records = cursor.fetchall()
     missing_records_column_names = [i[0] for i in cursor.description]
@@ -48,8 +47,7 @@ def upsert_table(cursor, table_name, dtypes_def):
         logger.debug(f"No new records for table", {"table_name": table_name})
         return
 
-    if table_name != "fpp_users":
-        df_mysql.set_index('id', inplace=True)
+    df_mysql.set_index('id', inplace=True)
 
     df_mysql = df_mysql.astype(dtypes_def)
 
@@ -65,14 +63,50 @@ def upsert_table(cursor, table_name, dtypes_def):
     # })
 
     # Merge new data from MySQL with existing data in Parquet
-    if table_name != "fpp_users":
-        df = pd.concat([df_mysql, df_parquet])
-        df.to_parquet(parquet_file)
-    else:
-        df_mysql.to_parquet(parquet_file)
+    df = pd.concat([df_mysql, df_parquet])
+    df.to_parquet(parquet_file)
 
     logger.info(f"Upserted records for in read model", {
         "table_name": table_name,
+        "amount_of_new_records": amount_of_new_records
+    })
+
+
+def upsert_users(cursor):
+    parquet_file = f"./data/fpp_users.parquet"
+
+    # Load existing Parquet file (if exists)
+    if os.path.isfile(parquet_file):
+        df_parquet = pd.read_parquet(parquet_file)
+        if not df_parquet.empty:
+            last_created_at = df_parquet['created_at'].max()
+        else:
+            last_created_at = None
+    else:
+        df_parquet = pd.DataFrame()
+        last_created_at = None
+
+    if last_created_at is not None:
+        cursor.execute(f"SELECT * FROM fpp_users WHERE created_at > '{last_created_at}' ORDER BY created_at DESC")
+    else:
+        cursor.execute("SELECT * FROM fpp_users ORDER BY created_at DESC")
+
+    missing_records = cursor.fetchall()
+    missing_records_column_names = [i[0] for i in cursor.description]
+    df_mysql = pd.DataFrame(missing_records, columns=missing_records_column_names)
+
+    amount_of_new_records = len(df_mysql)
+
+    if amount_of_new_records == 0:
+        logger.debug(f"No new records for table", {"table_name": "fpp_users"})
+        return
+
+    # Merge new data from MySQL with existing data in Parquet
+    df = pd.concat([df_mysql, df_parquet])
+    df.to_parquet(parquet_file)
+
+    logger.info(f"Upserted records for in read model", {
+        "table_name": "fpp_users",
         "amount_of_new_records": amount_of_new_records
     })
 
@@ -98,4 +132,5 @@ def update_read_model():
     upsert_table(cursor, "fpp_votes", {'room_id': 'int16', 'min_estimation': 'int16', 'max_estimation': 'int16',
                                        'amount_of_estimations': 'int16', 'amount_of_spectators': 'int16',
                                        'duration': 'int16'})
-    upsert_table(cursor, "fpp_users", {})
+
+    upsert_users(cursor)
