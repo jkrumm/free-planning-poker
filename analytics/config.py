c60d2274ed02b03b45f577856bfe064082894b14
jkrumm <jkrumm@pm.me>
2024-06-01 13:00:28 +0200

    feat(analytics): daily analytics email with Cron

--- analytics/config.py
+++ analytics/config.py
@@ -2,3 +2,5 @@ import os
 
 ANALYTICS_SECRET_TOKEN = os.getenv("ANALYTICS_SECRET_TOKEN")
 DATA_DIR = os.getenv('DATA_DIR', './data')
+BEA_BASE_URL = os.getenv('BEA_BASE_URL')
+BEA_SECRET_KEY = os.getenv('BEA_SECRET_KEY')
