90bd8a3ef00facf6903d24357086892b2e2ed727
jkrumm <jkrumm@pm.me>
2024-06-02 12:59:47 +0200

    feat(analytics): add tooltip to historical chart

--- src/components/analytics/historical-chart-options.ts
+++ src/components/analytics/historical-chart-options.ts
@@ -1,5 +1,53 @@
 import type { AgChartOptions } from 'ag-charts-community';
 
+function renderer(
+  params: {
+    datum: Record<string, number>;
+    xKey: string;
+    yKey: string;
+  },
+  title: string,
+  color: string,
+) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const value = params.datum[params.yKey].toFixed(0);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const date = new Date(params.datum[params.xKey]);
+
+  // Define an array of month names
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  // Get the month, day, and year from the date
+  const monthName = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  // Format the date as "May 19 2024"
+  const formattedDate = `${monthName} ${day} ${year}`;
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-argument
+    content: `${value} on ${formattedDate}`,
+    color: '#fff',
+    title,
+    backgroundColor: color,
+  };
+}
+
 class HistoricalChartOptions {
   showDaily = true;
   showAcc = true;
@@ -53,10 +101,10 @@ class HistoricalChartOptions {
         enabled: false,
       },
       navigator: {
-        enabled: true,
+        enabled: false,
       },
       tooltip: {
-        enabled: false,
+        enabled: true,
       },
       series: [
         {
@@ -68,6 +116,10 @@ class HistoricalChartOptions {
           visible: this.showEstimations && this.showDaily,
           fill: '#40C057',
           fillOpacity: this.showAcc ? 0.2 : 1,
+          tooltip: {
+            renderer: (params) =>
+              renderer(params, 'Estimations Daily', '#40C057'),
+          },
         },
         {
           type: 'line',
@@ -80,6 +132,10 @@ class HistoricalChartOptions {
           marker: {
             enabled: false,
           },
+          tooltip: {
+            renderer: (params) =>
+              renderer(params, 'Estimations Acc', '#40C057'),
+          },
         },
         {
           type: 'bar',
@@ -90,6 +146,9 @@ class HistoricalChartOptions {
           visible: this.showVotes && this.showDaily,
           fill: '#1971C2',
           fillOpacity: this.showAcc ? 0.2 : 1,
+          tooltip: {
+            renderer: (params) => renderer(params, 'Votes Daily', '#1971C2'),
+          },
         },
         {
           type: 'line',
@@ -102,6 +161,9 @@ class HistoricalChartOptions {
           marker: {
             enabled: false,
           },
+          tooltip: {
+            renderer: (params) => renderer(params, 'Votes Acc', '#1971C2'),
+          },
         },
         {
           type: 'bar',
@@ -112,6 +174,10 @@ class HistoricalChartOptions {
           visible: this.showPageViews && this.showDaily,
           fill: '#FA5252',
           fillOpacity: this.showAcc ? 0.2 : 1,
+          tooltip: {
+            renderer: (params) =>
+              renderer(params, 'Pages Views Daily', '#FA5252'),
+          },
         },
         {
           type: 'line',
@@ -124,27 +190,38 @@ class HistoricalChartOptions {
           marker: {
             enabled: false,
           },
+          tooltip: {
+            renderer: (params) => renderer(params, 'Page Views Acc', '#FA5252'),
+          },
         },
         {
           type: 'bar',
           xKey: 'date',
           yKey: 'new_users',
-          yName: 'New Users Daily',
+          yName: 'Unique Users Daily',
           stacked: true,
           visible: this.showNewUsers && this.showDaily,
           fill: '#FAB005',
           fillOpacity: this.showAcc ? 0.2 : 1,
+          tooltip: {
+            renderer: (params) =>
+              renderer(params, 'Unique Users Daily', '#FAB005'),
+          },
         },
         {
           type: 'line',
           xKey: 'date',
           yKey: 'acc_new_users',
-          yName: 'New Users Acc',
+          yName: 'Unique Users Acc',
           visible: this.showAccNewUsers && this.showAcc,
           strokeWidth: 2,
           stroke: '#FAB005',
           marker: {
             enabled: false,
+          },
+          tooltip: {
+            renderer: (params) =>
+              renderer(params, 'Unique Users Acc', '#FAB005'),
           },
         },
       ],
