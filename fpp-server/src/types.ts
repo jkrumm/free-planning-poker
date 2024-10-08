561a2344ea12ab13832f900eff64c040d9c1c7ea
jkrumm <jkrumm@pm.me>
2024-10-08 20:48:24 +0200

    feat(v6): extended analytics with timestamps

---
+++ fpp-server/src/types.ts
@@ -0,0 +1,23 @@
+export interface AnalyticsUser {
+  estimation: any;
+  isSpectator: boolean;
+  firstActive: number;
+  firstActiveReadable: string;
+  lastActive: number;
+  lastActiveReadable: string;
+}
+
+export interface Analytics {
+  connectedUsers: number;
+  openRooms: number;
+  rooms: {
+    userCount: number;
+    firstActive: number;
+    firstActiveReadable: string;
+    lastActive: number;
+    lastActiveReadable: string;
+    lastUpdated: number;
+    lastUpdatedReadable: string;
+    users: AnalyticsUser[];
+  }[];
+}
