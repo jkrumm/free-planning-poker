a74a31ee9cd2c1f4ef96a98c3fa1363cf408b471
jkrumm <jkrumm@pm.me>
2025-02-04 13:45:06 +0100

    feat(v7): update favicon, logo, and web app manifest assets

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -46,25 +46,20 @@ export const Meta = ({
       <meta property="og:image:alt" content="Free Planning Poker" />
       <meta charSet="utf-8" />
       <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
         rel="apple-touch-icon"
         sizes="180x180"
         href="/apple-touch-icon.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
+      <meta name="apple-mobile-web-app-title" content="FPP" />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1971c2" />
-      <meta name="msapplication-TileColor" content="#1a1b1e" />
       <meta name="theme-color" content="#1a1b1e" />
       <meta name="darkreader-lock" />
       <ColorSchemeScript defaultColorScheme="dark" />
