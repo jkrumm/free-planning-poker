fix(analytics): handle NaN values in room statistics calculation

analytics/room/calc_room_stats.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+import numpy as np
 
 from config import DATA_DIR
 from scripts.update_read_model import update_votes_read_model
@@ -26,6 +27,8 @@ def calc_room_stats(room_id):
 
     # average duration
     duration = r(votes['duration'].mean(), 0)
+    if np.isnan(duration):
+        duration = 0
 
     # count of estimations
     estimations = int(votes['amount_of_estimations'].sum())
@@ -35,21 +38,33 @@ def calc_room_stats(room_id):
         estimations_per_vote = 0
     else:
         estimations_per_vote = r(estimations / total_votes)
+        if np.isnan(estimations_per_vote):
+            estimations_per_vote = 0
 
     # average min estimation
     avg_min_estimation = r(votes['min_estimation'].mean())
+    if np.isnan(avg_min_estimation):
+        avg_min_estimation = 0
 
     # average avg estimation
     avg_avg_estimation = r(votes['avg_estimation'].mean())
+    if np.isnan(avg_avg_estimation):
+        avg_avg_estimation = 0
 
     # average max estimation
     avg_max_estimation = r(votes['max_estimation'].mean())
+    if np.isnan(avg_max_estimation):
+        avg_max_estimation = 0
 
     # count of spectators
     spectators = int(votes['amount_of_spectators'].sum())
 
     # spectators per vote
-    spectators_per_vote = r(spectators / total_votes)
+    spectators_per_vote = 0
+    if total_votes > 0:
+        spectators_per_vote = r(spectators / total_votes)
+        if np.isnan(spectators_per_vote):
+            spectators_per_vote = 0
 
     return {
         "votes": total_votes,